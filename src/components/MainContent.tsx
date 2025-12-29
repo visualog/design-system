58220cac62c029bc24ca15ec3c2ff4555a83acb2
visualog <taikyong@gmail.com>
2025-12-30 08:42:28 +0900

    Feat: Center main article content horizontally
    Added 'mx-auto' to the <article> tag within src/components/MainContent.tsx to horizontally center it when the browser width is wide, as requested by the user.

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -8,7 +8,7 @@ interface MainContentProps {
 const MainContent: React.FC<MainContentProps> = ({ children }) => {
   return (
     <main className="flex-1 pt-8 md:p-12 lg:p-16 md:pl-60">
-      <article className="max-w-3xl">
+      <article className="max-w-3xl mx-auto">
         <Breadcrumb />
         {children}
       </article>
