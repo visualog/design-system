b35469e23ddf1da653f8ac74401a2f633a95789a
visualog <taikyong@gmail.com>
2025-12-30 08:41:45 +0900

    Feat: Set max-width of main article to max-w-3xl (768px)
    Applied 'max-w-3xl' to the <article> tag within src/components/MainContent.tsx, adhering to the user's request to use Tailwind-defined spacing units for a max-width of 768px.

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -8,7 +8,7 @@ interface MainContentProps {
 const MainContent: React.FC<MainContentProps> = ({ children }) => {
   return (
     <main className="flex-1 pt-8 md:p-12 lg:p-16 md:pl-60">
-      <article>
+      <article className="max-w-3xl">
         <Breadcrumb />
         {children}
       </article>
