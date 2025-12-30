17c45cac724ce155c56d11920da0062f25cd0b00
visualog <taikyong@gmail.com>
2025-12-30 10:33:22 +0900

    Revert "Fix: Include alpha levels 10-90 in Alpha section display"
    This reverts commit 44e007266bc0b4b5b609e8c04fcbea54903b6fa4.

--- src/components/ColorPaletteDisplay.tsx
+++ src/components/ColorPaletteDisplay.tsx
@@ -107,7 +107,8 @@ const ColorPaletteDisplay: React.FC = () => {
 
   const allShades = Object.values(colors.palette).flat();
   const unwantedLevels = [
-    // These levels represent alpha variations (opacity 10% to 90%) and should now be included
+    "10 (10%)", "20 (20%)", "30 (30%)", "40 (40%)", "50 (50%)",
+    "60 (60%)", "70 (70%)", "80 (80%)", "90 (90%)"
   ];
   const allLevels = [...new Set(allShades.map(s => s.level))].filter(level => !unwantedLevels.includes(level));
 
