a78a8eae564467d3d4a1515bd520ca5910ec5a19
visualog <taikyong@gmail.com>
2026-02-03 09:34:47 +0900

    fix: update base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/design-system/',
+  base: '/',
   plugins: [
     svgr({ include: '**/*.svg' }),
     react(),
