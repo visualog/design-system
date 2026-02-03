615c15b1a9ce66a3eb97af7398ec068d3c5421b8
visualog <taikyong@gmail.com>
2026-02-03 09:39:26 +0900

    chore: force rebuild to apply base path change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/', // Ensure root path for Vercel deployment
   plugins: [
     svgr({ include: '**/*.svg' }),
     react(),
